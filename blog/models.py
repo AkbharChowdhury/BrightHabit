44ff5e5d79d35bdb023b7d52dcb8f65d0ae9dbc4
Akbhar <112480476+AkbharChowdhury@users.noreply.github.com>
2024-04-23 17:50:24 +0100

    c

--- blog/models.py
+++ blog/models.py
@@ -1,3 +1,14 @@
+from django.contrib.auth.models import User
+from django.utils import timezone
 from django.db import models
+from ckeditor.fields import RichTextField
 
-# Create your models here.
+
+class Post(models.Model):
+    title = models.CharField(max_length=100)
+    # body = models.TextField()
+    body = RichTextField(blank=True, null=True)
+    date_posted = models.DateTimeField(default=timezone.now)
+    author = models.ForeignKey(User, on_delete=models.CASCADE)
+    image = models.ImageField(null=True, blank=True, upload_to='images/')
+
