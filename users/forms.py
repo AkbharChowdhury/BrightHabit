8b288cbb07f282f7e2beffa783433b44920b0b13
Akbhar <112480476+AkbharChowdhury@users.noreply.github.com>
2024-04-24 17:49:15 +0100

    d

--- users/forms.py
+++ users/forms.py
@@ -1,6 +1,7 @@
 from django import forms
 from django.contrib.auth.forms import UserCreationForm
 from django.contrib.auth.models import User
+from django.core.exceptions import ValidationError
 
 
 class UserRegisterForm(UserCreationForm):
@@ -9,3 +10,9 @@ class UserRegisterForm(UserCreationForm):
     class Meta:
         model = User
         fields = ('username', 'email', 'password1', 'password2')
+
+    def clean_email(self):
+        email = self.cleaned_data["email"]
+        if User.objects.filter(email=email).exists():
+            raise ValidationError("A user with this email already exists!")
+        return email
--- users/views.py
+++ users/views.py
@@ -24,22 +24,21 @@ class RegisterView(CreateView):
             username = form.cleaned_data.get('username')
             messages.success(request, f'Account created for {username}. you can now login')
             return redirect(reverse_lazy('login'))
-        else:
-            return render(request, 'users/register.html')
+
+        return render(request, 'users/register.html', {'form': form})
 
 
 
-
-def register(request):
-    form = UserRegisterForm(request.POST) if request.method == 'POST' else UserRegisterForm()
-    if request.method == 'POST':
-        if form.is_valid():
-            form.save()
-            username = form.cleaned_data.get('username')
-            messages.success(request, f'Account created for {username}. you can now login')
-            return redirect('login')
-
-    return render(request, 'users/register.html', {'form': form})
+# def register(request):
+#     form = UserRegisterForm(request.POST) if request.method == 'POST' else UserRegisterForm()
+#     if request.method == 'POST':
+#         if form.is_valid():
+#             form.save()
+#             username = form.cleaned_data.get('username')
+#             messages.success(request, f'Account created for {username}. you can now login')
+#             return redirect('login')
+#
+#     return render(request, 'users/register.html', {'form': form})
 
 
 @login_required
