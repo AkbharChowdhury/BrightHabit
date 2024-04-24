3e6e1c1b55cc6728715da23339ed9b3414be5efa
Akbhar <112480476+AkbharChowdhury@users.noreply.github.com>
2024-04-24 15:06:12 +0100

    d

--- BrightHabit/settings.py
+++ BrightHabit/settings.py
@@ -124,7 +124,7 @@ USE_TZ = True
 STATIC_URL = 'static/'
 MEDIA_URL = '/media/'
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
-
+LOGIN_REDIRECT_URL = 'blog_index'
 
 
 # Default primary key field type
