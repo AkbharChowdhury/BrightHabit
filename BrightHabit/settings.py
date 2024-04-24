f60c000a3e47e801f46e4864edcd4ab3cc8cf1c6
Akbhar <112480476+AkbharChowdhury@users.noreply.github.com>
2024-04-24 11:52:35 +0100

    x

--- BrightHabit/settings.py
+++ BrightHabit/settings.py
@@ -37,7 +37,7 @@ INSTALLED_APPS = [
     'django.contrib.staticfiles',
     'django.contrib.humanize',
     'blog.apps.BlogConfig',
-
+    'users.apps.UsersConfig',
     "crispy_forms",
     "crispy_bootstrap5",
 
