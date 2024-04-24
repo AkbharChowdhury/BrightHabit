1eb276c95d0956a3e55cfdff0a21d9ca3a64bf0d
Akbhar <112480476+AkbharChowdhury@users.noreply.github.com>
2024-04-24 14:51:28 +0100

    d

--- users/views.py
+++ users/views.py
@@ -1,16 +1,17 @@
 from django.shortcuts import render, redirect
 from django.contrib.auth.forms import UserCreationForm
 from django.contrib import messages
+from django.contrib.auth.models import User
+from .forms import UserRegisterForm
 
 
 def register(request):
+    form = UserRegisterForm(request.POST) if request.method == 'POST' else UserRegisterForm()
     if request.method == 'POST':
-        form = UserCreationForm(request.POST)
         if form.is_valid():
+            form.save()
             username = form.cleaned_data.get('username')
             messages.success(request, f'Account created for {username}')
-            form.save()
             return redirect('blog_index')
-    else:
-        form = UserCreationForm()
+
     return render(request, 'users/register.html', {'form': form})
