a1196389afe4780df716f7cfcf35da28bb83036a
Akbhar <112480476+AkbharChowdhury@users.noreply.github.com>
2024-04-27 08:16:37 +0100

    d

--- users/views.py
+++ users/views.py
@@ -1,11 +1,9 @@
 from django.contrib import messages
-from django.contrib.auth.decorators import login_required
+from django.contrib.auth.mixins import LoginRequiredMixin
 from django.shortcuts import render, redirect
 from django.urls import reverse_lazy
 from django.views.generic import CreateView
-
 from .forms import UserRegisterForm, UserUpdateForm, ProfileUpdateForm
-from django.contrib.auth.mixins import LoginRequiredMixin
 
 
 class RegisterView(CreateView):
