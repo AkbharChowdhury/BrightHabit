a161024820ce894388355ecbb798e52f25b96e6a
Akbhar <112480476+AkbharChowdhury@users.noreply.github.com>
2024-09-29 13:44:24 +0100

    done

--- blog/models.py
+++ blog/models.py
@@ -6,12 +6,21 @@ from ckeditor.fields import RichTextField
 from django.urls import reverse
 
 
+class Tag(models.Model):
+    name = models.CharField(max_length=100)
+
+    def __str__(self):
+        return self.name
+
+
 class Post(models.Model):
     title = models.CharField(max_length=100)
     body = RichTextField(blank=True, null=True)
     date_posted = models.DateTimeField(default=timezone.now)
     author = models.ForeignKey(User, on_delete=models.CASCADE)
     image = models.ImageField(null=True, blank=True, upload_to='images/')
+    tags = models.ManyToManyField(Tag, blank=True, related_name='tags')
+
     search_fields = ('title', 'body', 'author')
 
     def __str__(self):
@@ -22,13 +31,13 @@ class Post(models.Model):
 
     @property
     def get_content(self):
-        return mark_safe(self.body
-                         .removeprefix('<pre>')
-                         .removesuffix('</pre>'))
+        return self.__clean_text(self.body)
 
     @property
     def snippet(self):
-        return mark_safe(f'{self.body
+        return f'{self.__clean_text(self.body[:200])}...'
+
+    def __clean_text(self, text):
+        return mark_safe(text
                          .removeprefix('<pre>')
-                         .removesuffix('</pre>')
-        [:200]}...')
+                         .removesuffix('</pre>'))
