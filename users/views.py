1e7c8250bb8cd48a098b3ae4280ba61102407044
Akbhar <112480476+AkbharChowdhury@users.noreply.github.com>
2024-11-23 07:55:03 +0000

    s

--- users/views.py
+++ users/views.py
@@ -24,48 +24,52 @@ class RegisterView(CreateView):
 
 class Profile(LoginRequiredMixin, CreateView):
     template_name = 'users/profile.html'
+    user_form = 'user_form'
+    profile_form = 'profile_form'
 
     def get(self, request, *args, **kwargs):
         return render(request, self.template_name, {
-            'u_form': UserUpdateForm(instance=request.user),
-            'p_form': ProfileUpdateForm(instance=request.user.profile)
+            self.user_form: UserUpdateForm(instance=request.user),
+            self.profile_form: ProfileUpdateForm(instance=request.user.profile)
         })
 
     def post(self, request, *args, **kwargs):
+        user_form = self.user_form
+        profile_fom = self.profile_form
         context = dict(
-            u_form=UserUpdateForm(request.POST, instance=request.user),
-            p_form=ProfileUpdateForm(request.POST, request.FILES, instance=request.user.profile)
+            user_form=UserUpdateForm(request.POST, instance=request.user),
+            profile_form=ProfileUpdateForm(request.POST, request.FILES, instance=request.user.profile)
         )
 
-        if context['u_form'].is_valid() and context['p_form'].is_valid():
-            context['u_form'].save()
-            context['p_form'].save()
+        if context[user_form].is_valid() and context[profile_fom].is_valid():
+            context[user_form].save()
+            context[profile_fom].save()
             messages.success(request, 'your profile has been updated!'.capitalize())
             return redirect('profile')
         messages.error(request, "Please fill all the fields")
         # return redirect('profile')
-        return render(request, 'users/profile.html', context)
+        return render(request, self.template_name, context)
 
-@login_required
-def profile(request):
-    if request.method == 'POST':
-        u_form = UserUpdateForm(request.POST, instance=request.user)
-        p_form = ProfileUpdateForm(request.POST,
-                                   request.FILES,
-                                   instance=request.user.profile)
-        if u_form.is_valid() and p_form.is_valid():
-            u_form.save()
-            p_form.save()
-            messages.success(request, f'Your account has been updated!')
-            return redirect('profile')
-
-    else:
-        u_form = UserUpdateForm(instance=request.user)
-        p_form = ProfileUpdateForm(instance=request.user.profile)
-
-    context = {
-        'u_form': u_form,
-        'p_form': p_form
-    }
-
-    return render(request, 'users/profile.html', context)
+# @login_required
+# def profile(request):
+#     if request.method == 'POST':
+#         u_form = UserUpdateForm(request.POST, instance=request.user)
+#         p_form = ProfileUpdateForm(request.POST,
+#                                    request.FILES,
+#                                    instance=request.user.profile)
+#         if u_form.is_valid() and p_form.is_valid():
+#             u_form.save()
+#             p_form.save()
+#             messages.success(request, f'Your account has been updated!')
+#             return redirect('profile')
+#
+#     else:
+#         u_form = UserUpdateForm(instance=request.user)
+#         p_form = ProfileUpdateForm(instance=request.user.profile)
+#
+#     context = {
+#         'u_form': u_form,
+#         'p_form': p_form
+#     }
+#
+#     return render(request, 'users/profile.html', context)
